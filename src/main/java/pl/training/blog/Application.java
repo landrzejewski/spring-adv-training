30472f236f970c4fb2746c7e6d5ed0d9c123d522
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-08 09:36:45 +0200

    Spring Data

--- src/main/java/pl/training/blog/Application.java
+++ src/main/java/pl/training/blog/Application.java
@@ -1,6 +1,12 @@
 package pl.training.blog;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.training.blog.commons.money.LocalMoney;
 import pl.training.blog.payments.application.PaymentRequest;
@@ -8,18 +14,22 @@ import pl.training.blog.payments.ports.usecases.ProcessPaymentUseCase;
 
 import static java.util.Collections.emptyMap;
 
+@SpringBootApplication
 @Log
-public class Application {
+@RequiredArgsConstructor
+public class Application implements ApplicationRunner {
 
-    private static final String BASE_PACKAGE = "pl.training.blog";
+    private final ProcessPaymentUseCase payments;
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BASE_PACKAGE)) {
-            var payments = context.getBean(ProcessPaymentUseCase.class);
-            var paymentRequest = new PaymentRequest(LocalMoney.of(1_000), emptyMap());
-            var payment = payments.process(paymentRequest);
-            log.info(payment.toString());
-        }
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var paymentRequest = new PaymentRequest(LocalMoney.of(1_000), emptyMap());
+        var payment = payments.process(paymentRequest);
+        log.info(payment.toString());
     }
 
 }
--- src/main/java/pl/training/blog/BlogConfiguration.java
+++ src/main/java/pl/training/blog/BlogConfiguration.java
@@ -1,25 +1,11 @@
 package pl.training.blog;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
-
-@EnableJpaRepositories(basePackages = "pl.training.blog")
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
 @Configuration
@@ -28,43 +14,6 @@ public class BlogConfiguration {
     @Bean
     public LocalValidatorFactoryBean validator() {
         return new LocalValidatorFactoryBean();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        var dataSource = new HikariDataSource();
-        dataSource.setUsername("admin");
-        dataSource.setPassword("admin");
-        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/blog");
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        return dataSource;
-    }
-
-    @Bean
-    public PropertiesFactoryBean jpaProperties() {
-        var factoryBean = new PropertiesFactoryBean();
-        factoryBean.setLocation(new ClassPathResource("jpa.properties"));
-        return factoryBean;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Properties jpaProperties) {
-        var factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setJpaProperties(jpaProperties);
-        factoryBean.setPackagesToScan("pl.training.blog");
-        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return factoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    @Bean
-    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
-        return new TransactionTemplate(transactionManager);
     }
 
 }
